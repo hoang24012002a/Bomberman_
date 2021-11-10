fc7e2b10479ced14663e84bb5dfceb8fe2de1b0c
tuan <tuanbigboss1@gmail.com>
2021-11-10 22:42:51 +0700

    create package enties, abstract class

---
+++ core/src/com/mygdx/game/entities/AnimatedEntity.java
@@ -0,0 +1,7 @@
+package com.mygdx.game.entities;
+
+/**
+ * abtract class của các đối tượng có animation.
+ */
+public abstract class AnimatedEntity extends Entity {
+}
---
+++ core/src/com/mygdx/game/entities/Entity.java
@@ -0,0 +1,4 @@
+package com.mygdx.game.entities;
+
+public abstract class Entity {
+}
