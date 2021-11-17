faad3687180c14b3ed7447574b98e51363e5301a
Truongvnu <th0375178055@gmail.com>
2021-11-17 22:14:42 +0700

    dang hoan thien

---
+++ core/src/com/mygdx/game/entities/StaticEntity/Bomb/Flame.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.entities.StaticEntity.Bomb;
+
+import com.mygdx.game.entities.AnimatedEntity;
+import com.mygdx.game.gamesys.GameManager;
+
+public class Flame extends AnimatedEntity {
+    public Flame(float x, float y){
+        super();
+        this.animation= GameManager.flameHorLeftLast;
+    }
+    // đang tìm cách để tạo hình cho flame
+    // kéo dài flame dựa vào FlameItem
+}
