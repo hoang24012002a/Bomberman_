95c10c0965b844c519a6de5815f4174a29783316
Truongvnu <th0375178055@gmail.com>
2021-11-17 22:18:02 +0700

    dang hoan thien

--- core/src/com/mygdx/game/entities/StaticEntity/Tile/Grass.java
+++ core/src/com/mygdx/game/entities/StaticEntity/Tile/Grass.java
@@ -1,26 +1,18 @@
 package com.mygdx.game.entities.StaticEntity.Tile;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.mygdx.game.entities.StaticEntities;
 import com.mygdx.game.gamesys.GameManager;
 
-
-public class Wall extends StaticEntities {
-    public Wall(float x, float y){
+public class Grass extends StaticEntities {
+    public Grass(float x, float y){
         super(x, y);
         this.canBreakable=false;
-        texture = GameManager.wall;
+        texture= GameManager.grass;
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
-    }
-
-    public void dispose() {
-        texture.dispose();
     }
 }
