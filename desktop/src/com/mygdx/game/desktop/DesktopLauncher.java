e2e3bc96d42c65358408fd77204e31a0aa652afe
= <=>
2021-11-22 17:00:28 +0700

    setting DesktopLauncher

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.mygdx.game.Bomb;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.x = 140;
+		config.y = 100;
 		config.height = 416;
 		config.width = 992;
 		new LwjglApplication(new Bomb(), config);
