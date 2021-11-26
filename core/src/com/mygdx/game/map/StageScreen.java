73f6e92468296724dd34715bcb1604cbe3224c00
= <=>
2021-11-26 23:34:54 +0700

    change getAt function, remove function, create addBomb function

--- core/src/com/mygdx/game/map/StageScreen.java
+++ core/src/com/mygdx/game/map/StageScreen.java
@@ -224,7 +224,8 @@ public class StageScreen extends Stage {
 
     // Function addBombg
     public void addBomb(Actor actor){
-        addActor(actor);
+        noActs.add(actor);
+        groupNoActs.addActor(actor);
     }
 
 }
