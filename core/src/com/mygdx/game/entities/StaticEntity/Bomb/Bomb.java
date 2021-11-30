d393aa8a68298b03a233821f18c647169ed4bb4a
Truongvnu <th0375178055@gmail.com>
2021-11-30 21:34:36 +0700

    test bomb exp then call flame

--- core/src/com/mygdx/game/entities/StaticEntity/Bomb/Bomb.java
+++ core/src/com/mygdx/game/entities/StaticEntity/Bomb/Bomb.java
@@ -12,9 +12,11 @@ public class Bomb extends AnimatedEntity {
     private boolean explored = false;
     protected final int timeToExplode = 3000; // 2000ms
     protected int numberOfBomb = 1;
+    private StageScreen stageScreen;
 
     public Bomb(float x, float y){
         super(x, y);
+        stageScreen = StageScreen.me;
         textureAtlas = GameManager.bombFlick.getKey();
         animation = GameManager.bombFlick.getValue();
     }
@@ -26,16 +28,34 @@ public class Bomb extends AnimatedEntity {
     private int dem = 0;
     @Override
     public void act(float delta) {
+        final Bomb _this = this;
         textureAtlas = GameManager.bombFlick.getKey();
         animation = GameManager.bombFlick.getValue();
         //TODO: nen lam theo cach nay.
+        //TODO: tại sao khi explore = true lại không hoạt động???
         dem++;
-        if (dem == 200) {
+        if (dem == 100 && getStage().getActors().notEmpty()) {
             dem = 0;
             explored = true;
             remove();
+            if(explored){
+                System.out.println("true");
+                stageScreen.addFlames(new FlameManager(positionX, positionY));
+            }
         }
-        return;
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try{
+//                    Thread.sleep(1000);
+//                    remove();
+//                    stageScreen.addFlames(new FlameManager(positionX, positionY));
+//                } catch (InterruptedException e){
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
+//        return;
     }
 
     public boolean isExplored() {
