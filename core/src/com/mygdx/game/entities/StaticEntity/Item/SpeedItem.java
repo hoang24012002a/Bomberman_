12e3a854571766a2fda2839fbe7b0b3c4c97692c
Truongvnu <th0375178055@gmail.com>
2021-12-03 23:39:43 +0700

    update method is broken

--- core/src/com/mygdx/game/entities/StaticEntity/Item/SpeedItem.java
+++ core/src/com/mygdx/game/entities/StaticEntity/Item/SpeedItem.java
@@ -13,6 +13,7 @@ public class SpeedItem extends Item {
     public SpeedItem(float positionX, float positionY){
         super(positionX, positionY);
         this.texture = GameManager.speedItem;
+        this.broken = false;
     }
 
     public SpeedItem(Brick brick){
@@ -31,6 +32,19 @@ public class SpeedItem extends Item {
             stageScreen.addActor(new Grass(positionX, positionY));
         }else{
             this.broken = false;
+        }
+    }
+
+    @Override
+    public boolean isBroken(){
+        if(remove()){
+            System.out.println("removeFlameItem");
+            stageScreen.remove(this);
+//            stageScreen.addActor(new Grass(positionX, positionY));
+            return true;
+        }else{
+            System.out.println("not yet remove");
+            return false;
         }
     }
 
